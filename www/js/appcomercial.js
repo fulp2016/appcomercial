4ae2afa05a90a76c3d672b761d0930e11d1a9009
fulp2016 <webmaster@fulp.es>
2016-05-03 08:16:21 +0100

    v1

---
+++ www/js/appcomercial.js
@@ -0,0 +1,43 @@
+function evolucion_insertas_formalizados() 
+{
+	alert('3ntra');
+	$('#ev-inserta').empty();
+	var serviceURL = "http://www.fulp.es/servicesfulp/evolucion_inserta_formalizados.json";
+	$.getJSON(serviceURL, function(data) {
+		//alert(data);
+		$('#ev-inserta').append( '<table class="table table-hover">'+
+									'<thead>'+                                  
+									'<tr>'+                                     
+                                      '<th>Mes</th>'+
+									  '<th>TOTAL 15</th>'+
+                                      '<th>TOTAL 16</th>'+
+									  '<th>ACUM 15</th>'+
+									  '<th>ACUM 16</th>'+                                   									  									  
+                                  '</tr>'+
+	                            '</thead>'+
+                              '<tbody>');
+		
+		/*$.each(data, function(index, dato) {
+			alert(dato.MES);
+			/*$('#ev-inserta').append('<tr>'+
+                                  '<td>'+dato.MES+'</td>'+
+								  '<td>'+dato.TOTAL_ANT+'</td>'+
+								  '<td>'+dato.TOTAL_ACT+'</td>'+
+								  '<td>'+'</td>'+
+								  '<td>'+'</td>'+							  								  								    
+								'</tr>'+
+								
+								'<tr>'+
+                                  '<td><strong>TOTAL</strong></td>'+
+								  '<td><strong></strong></td>'
+								  '<td><strong></strong></td>'+
+								  '<td><strong></strong></td>'+
+								'</tr>');*/
+
+		/*});*/
+		
+		$('#ev-inserta').append( '</tbody>'+  
+								'</table>');
+
+	});
+}
