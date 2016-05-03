901e95eb754217648eeea1faa8491ebd18690489
fulp2016 <webmaster@fulp.es>
2016-05-03 11:17:58 +0100

    v1

--- www/js/appcomercial.js
+++ www/js/appcomercial.js
@@ -1,37 +1,64 @@
 function evolucion_insertas_formalizados() 
 {
+	var mesact = (getMonth()+1);
+	var acumact = 0;
+	var acumant = 0;
+	var total_act = 0;
+	var total_ant = 0;
+	var total_acumact = 0;
+	var total_acumant = 0;
 	$('#ev-inserta').empty();
 	var serviceURL = "http://www.fulp.es/servicesfulp/evolucion_inserta_formalizados.json";
 	$.getJSON(serviceURL, function(data) {
 
-		$('#ev-inserta').append( '<table class="table table-hover">'+                                 
+		$('#ev-inserta').append( '<table class="table table-hover">'+
+									'<thead>'+                                  
 									'<tr>'+                                     
-                                      '<td>Mes</td>'+
-									  '<td>TOTAL 15</td>'+
-                                      '<td>TOTAL 16</td>'+
-									  '<td>ACUM 15</td>'+
-									  '<td>ACUM 16</td>'+                                   									  									  
-                                  '</tr>');
+                                      '<th>Mes</th>'+
+									  '<th>TOTAL 15</th>'+
+                                      '<th>TOTAL 16</th>'+
+									  '<th>ACUM 15</th>'+
+									  '<th>ACUM 16</th>'+                                   									  									  
+                                  '</tr>'+
+	                            '</thead>'+
+                              '<tbody>');
 		
 		$.each(data, function(index, dato) {
+			
+			if(dato.MES<mesact)
+			{
+				acumact = acumact + dato.TOTAL_ACT;
+				acumant = acumact + dato.TOTAL_ANT;
+			}
+			else 
+			{
+				acumact = '';
+				acumant = '';
+			}
 			
 			$('#ev-inserta').append( '<tr>'+
                                   '<td>'+dato.MES+'</td>'+
 								  '<td>'+dato.TOTAL_ANT+'</td>'+
 								  '<td>'+dato.TOTAL_ACT+'</td>'+
-								  '<td>'+'</td>'+
-								  '<td>'+'</td>'+							  								  								    
+								  '<td>'+acumant+'</td>'+
+								  '<td>'+acumact+'</td>'+							  								  								    
 								'</tr>');
+								
+			total_act = total_act + dato.TOTAL_ACT;	
+			total_ant = total_ant + dato.TOTAL_ANT;
+			total_acumact = total_acumact + acumact;
+			total_acumant = total_acumant + acumant;
 
 		});
 		
 		$('#ev-inserta').append( '<tr>'+
                                   '<td><strong>TOTAL</strong></td>'+
-								  '<td><strong></strong></td>'+
-								  '<td><strong></strong></td>'+
-								  '<td><strong></strong></td>'+
-								  '<td><strong></strong></td>'+
+								  '<td><strong>'+total_ant+'</strong></td>'+
+								  '<td><strong>'+total_act+'</strong></td>'+
+								  '<td><strong>'+total_acumact+'</strong></td>'+
+								  '<td><strong>'+total_acumact+'</strong></td>'+
 								'</tr>'+
+								'</tbody>'+  
 								'</table>');
 
 	});
